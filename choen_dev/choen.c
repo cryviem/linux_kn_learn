b97c79c75cca3d725138e8001d8662ef6e023d37
Nguyen Hong An <an23593@gmail.com>
2023-03-12 22:43:50 +0700

    restruct repo

---
+++ choen_dev/choen.c
@@ -0,0 +1,22 @@
+#include <linux/module.h>
+#include <linux/version.h>
+#include <linux/kernel.h>
+
+
+static int __init choen_init(void) /* Constructor */
+{
+    printk(KERN_INFO "Hello: choen registered\n");
+    return 0;
+}
+
+static void __exit choen_exit(void) /* Destructor */
+{
+    printk(KERN_INFO "Goodbye: choen unregistered\n");
+}
+
+module_init(choen_init);
+module_exit(choen_exit);
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Nguyen Hong An");
+MODULE_DESCRIPTION("Hello world ldd");
+MODULE_INFO(intree, "Y");
