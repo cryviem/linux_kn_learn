723df9ab0f8e1aec82cc068420971b22c23a881b
Nguyen Hong An <an23593@gmail.com>
2024-04-14 16:01:11 +0700

    done platform0: basic platform device and driver matching roughly via name

--- platform0/choen_device_simulate.c
+++ platform0/choen_device_simulate.c
@@ -7,12 +7,9 @@ This module aim to register those pseudo platform devices
 #include <linux/version.h>
 #include <linux/kernel.h>
 #include <linux/platform_device.h>
+#include "choen_common.h"
 
 /* let make some private data for device */
-struct choen_dev_priv {
-    const char * serial;
-};
-
 struct choen_dev_priv choen_priv_table[2] = 
 {
     {.serial = "CHOENSN0"},
@@ -22,7 +19,7 @@ struct choen_dev_priv choen_priv_table[2] =
 /* this release function aim to free up those private data if any*/
 void choen_dev_release(struct device* pdev)
 {
-    printk(KERN_INFO "choen_dev_release > should clean up those private data\n");
+    pr_info("choen_dev_release > should clean up those private data\n");
 }
 
 /* create this struct to carry all devices information */
@@ -45,7 +42,7 @@ static int __init choen_init(void) /* Constructor */
     /* register platform device to kernel */
     platform_device_register(&choen_dev0);
     platform_device_register(&choen_dev1);
-    printk(KERN_INFO "Hello: choen device simulation loaded\n");
+    pr_info("Hello: choen device simulation loaded\n");
 
     return 0;
 }
@@ -55,12 +52,12 @@ static void __exit choen_exit(void) /* Destructor */
     /* do reverse, unregister*/
     platform_device_unregister(&choen_dev0);
     platform_device_unregister(&choen_dev1);
-    printk(KERN_INFO "Goodbye: choen device simulation unloaded\n");
+    pr_info("Goodbye: choen device simulation unloaded\n");
 }
 
 module_init(choen_init);
 module_exit(choen_exit);
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Nguyen Hong An");
-MODULE_DESCRIPTION("Hello world ldd");
+MODULE_DESCRIPTION("Submit 2 dummy platform devices");
 MODULE_INFO(intree, "Y");
--- platform0/choen_driver.c
+++ platform0/choen_driver.c
@@ -1,18 +1,39 @@
 #include <linux/module.h>
 #include <linux/version.h>
 #include <linux/kernel.h>
+#include <linux/errno.h>
 #include <linux/platform_device.h>
-
+#include "choen_common.h"
 
 int choen_probe(struct platform_device* pdev)
 {
-    printk(KERN_INFO "choen_probe is called\n");
+    struct choen_dev_priv *pdev_prv_data;
+
+    pr_info("choen_probe is called\n");
+    /* get private data from device */
+    pdev_prv_data = (struct choen_dev_priv*)dev_get_platdata(&pdev->dev);
+    if (pdev_prv_data == NULL)
+    {
+        pr_warn("choen_probe > no device private data found\n");
+        return -EINVAL;
+    }
+
+    pr_info("choen_probe > connected to device serial: %s\n", pdev_prv_data->serial);
     return 0;
 }
 
 int choen_remove(struct platform_device* pdev)
 {
-    printk(KERN_INFO "choen_remove is called\n");
+    struct choen_dev_priv *pdev_prv_data;
+
+    pr_info("choen_remove is called\n");
+
+    pdev_prv_data = (struct choen_dev_priv*)dev_get_platdata(&pdev->dev);
+    if (pdev_prv_data != NULL)
+    {
+        pr_info("choen_probe > disconnecting from device serial: %s\n", pdev_prv_data->serial);
+    }
+    
     return 0;
 }
 
@@ -25,14 +46,14 @@ struct platform_driver choen_driver =
 
 static int __init choen_init(void) /* Constructor */
 {
-    printk(KERN_INFO "Hello: choen driver loaded\n");
+    pr_info("Hello: choen driver loaded\n");
     platform_driver_register(&choen_driver);
     return 0;
 }
 
 static void __exit choen_exit(void) /* Destructor */
 {
-    printk(KERN_INFO "Goodbye: choen driver unloaded\n");
+    pr_info("Goodbye: choen driver unloaded\n");
     platform_driver_unregister(&choen_driver);
 }
 
@@ -40,5 +61,5 @@ module_init(choen_init);
 module_exit(choen_exit);
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Nguyen Hong An");
-MODULE_DESCRIPTION("Hello world ldd");
+MODULE_DESCRIPTION("Simple platform driver");
 MODULE_INFO(intree, "Y");
